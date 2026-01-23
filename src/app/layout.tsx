18b938c48a4e59503262a020edea0d11b8f277de
Mayank Saini <mayanksaraswal@gmail.com>
2026-01-23 13:04:31 +0530

    setup clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter, } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import {dark} from '@clerk/themes'
 import "./globals.css";
 
 const inter = Inter({
@@ -25,14 +34,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+    <ClerkProvider 
+    appearance={{
+      theme: dark,
+    }}
+    >
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <header>
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+            <button className="bg-rose-500 text-white p-2 rounded">
+              Sign Up
+            </button>
+            </SignUpButton>
+            
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+          </header>
+          
           {children}
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
